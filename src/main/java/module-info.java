aed3b1af451fcb70456837089540aa2849b63f37
Kupcor <Kupcor1998@gmail.com>
2023-02-27 19:55:23 +0100

    Completion of several mechanisms (check, checkmate, en passant, castling, etc.), improvement of the king movement mechanism, some class refactoring (still too large), the first tests have been written.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,9 @@ module org.pk {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires javafx.base;
 
     opens org.pk to javafx.fxml;
     exports org.pk;
+    exports org.pk.chessboard;
 }
---
+++ test/java/org/pk/chessboard/FieldTest.java
@@ -0,0 +1,46 @@
+package org.pk.chessboard;
+
+import javafx.application.Platform;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class FieldTest {
+
+    @BeforeAll
+    static  void setUp() {
+        //  Need to initialize JFX environment in order to test Objects/Classes that use JFX components
+        //  In this case figure objects use JFX Label component
+        Platform.startup(() -> {});
+    }
+
+    @Test
+    void testFieldConstructor() {
+        Field testWhiteField = new Field(48, 48,  0, 0);
+        Field testBlackField = new Field(48,48,7, 0);
+
+        Assertions.assertEquals("#a89187", testWhiteField.getBackgroundColor());
+        Assertions.assertNotEquals(testWhiteField.getBackgroundColor(), "#613d2e");
+
+        Assertions.assertEquals("#613d2e", testBlackField.getBackgroundColor());
+        Assertions.assertNotEquals("#a89187", testBlackField.getBackgroundColor());
+
+        Assertions.assertEquals(7, testBlackField.getVerticalPosition());
+        Assertions.assertEquals(0, testBlackField.getHorizontalPosition());
+
+        Assertions.assertEquals(0, testWhiteField.getVerticalPosition());
+        Assertions.assertEquals(0, testWhiteField.getHorizontalPosition());
+
+        //  Field do not contain any figures when it is initialized
+        Assertions.assertTrue(testBlackField.getChildren().isEmpty());
+        Assertions.assertTrue(testWhiteField.getChildren().isEmpty());
+
+        Assertions.assertNull(testWhiteField.getFigure());
+        Assertions.assertNull(testBlackField.getFigure());
+    }
+
+    @Test
+    void testSetFigure() {
+
+    }
+}
